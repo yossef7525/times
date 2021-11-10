877f5786e8e96ea2fe3cd7eafa5d757303eb9c97
yossi7525 <a0548475725@gmail.com>
2021-11-10 17:02:02 +0200

    שככק'

--- src/app/comps/c1/c1.component.ts
+++ src/app/comps/c1/c1.component.ts
@@ -12,28 +12,39 @@ import { AjaxService } from 'src/app/services/ajax.service';
   styleUrls: ['./c1.component.css']
 })
 export class C1Component implements OnInit {
-
+  myDate = new Date()
+  public timeA = new Date()
+  public time = new Date()
+  public timeB!: number
+  timeron: boolean = false;
+  panelOpenState = false;
   public user: SocialUser = new SocialUser;
-  data:Data[]=[]
-  constructor(public mvc: AjaxService , private authService: SocialAuthService, private router: Router) { }
+  data: Data[] = []
+  constructor(public mvc: AjaxService, private authService: SocialAuthService, private router: Router) { }
 
   ngOnInit(): void {
     this.authService.authState.subscribe(user => {
-    this.user = user})
-    this.mvc.httpget("https://raw.githubusercontent.com/yossef7525/angular-router/master/src/data.json")
-    .subscribe(response => {
-      this.data = response["data"]
-      console.log(this.data);
-      
+      this.user = user
     })
+    this.mvc.httpget("https://raw.githubusercontent.com/yossef7525/angular-router/master/src/data.json")
+      .subscribe(response => {
+        this.data = response["data"]
+        console.log(this.data);
+
+      })
+  }
+  public starttimer(): void {
+    this.timeron = true;
+    this.timeA = new Date();
+    setInterval(() => { this.time = new Date(), this.timeB = this.time.getTime() - this.timeA.getTime() }, 1);
   }
   public async signOut(): Promise<void> {
 
-   await this.authService.signOut(true);
+    await this.authService.signOut(true);
     console.log('this.mvc.loading:', this.mvc.loading);
     this.mvc.loading = false;
     console.log('this.mvc.loading:', this.mvc.loading);
     this.router.navigate(['/login'])
-    
+
   }
 }
