ed76a3357ad7eb107bc9770687953719b0717346
yossef7525 <a0548475725@gmail.com>
2021-11-10 20:24:11 +0200

    fwef

--- src/app/comps/c1/c1.component.ts
+++ src/app/comps/c1/c1.component.ts
@@ -13,9 +13,10 @@ import { AjaxService } from 'src/app/services/ajax.service';
 })
 export class C1Component implements OnInit {
   myDate = new Date()
+  public timer:any
   public timeA = new Date()
   public time = new Date()
-  public timeB!: number
+  public timeB!: string
   timeron: boolean = false;
   panelOpenState = false;
   public user: SocialUser = new SocialUser;
@@ -36,7 +37,34 @@ export class C1Component implements OnInit {
   public starttimer(): void {
     this.timeron = true;
     this.timeA = new Date();
-    setInterval(() => { this.time = new Date(), this.timeB = this.time.getTime() - this.timeA.getTime() }, 1);
+  this.timer = setInterval(() => { 
+      this.time = new Date();
+       var ms = this.time.getTime() - this.timeA.getTime()
+       
+      var seconds = Math.floor((ms / 1000) %60)
+      var minutes = Math.floor((ms / (1000 * 60)) %60)
+      var hours = Math.floor((ms / (1000 * 60 * 60)) %24)
+      hours = (hours < 10) ? Number(`0${hours}`) : hours
+      minutes = (minutes < 10) ? Number(`0${minutes}`) : minutes
+      seconds = (seconds < 10) ? Number(`0${seconds}`) : seconds
+      console.log(hours,minutes, seconds);
+      
+      this.timeB = `${hours}:${minutes}:${seconds}`
+      }, 1000);
+  }
+  public offttimer(): void {
+    
+    var req = {
+      data: `${this.myDate.getDate()}/${this.myDate.getMonth()+1}/${this.myDate.getFullYear()}`,
+      timestart: this.timeA,
+      timeend: this.time,
+      sumtime: this.timeB,
+      seconds: this.time.getTime() - this.timeA.getTime()
+    }
+    console.log('req:', req);
+    // http.post('/???????', req)....
+    this.timeron = false;
+    clearInterval(this.timer);
   }
   public async signOut(): Promise<void> {
 
