d2bf25004e70e20ed1e4558cb62942560c8b8479
yossi7525 <a0548475725@gmail.com>
2021-11-15 16:43:06 +0200

    גדעע

--- src/app/comps/timerpage/timerpage.component.ts
+++ src/app/comps/timerpage/timerpage.component.ts
@@ -8,61 +8,62 @@ import { AjaxService } from 'src/app/services/ajax.service';
 })
 export class TimerpageComponent implements OnInit {
   myDate = new Date()
-  public timer:any
+  public timer: any
   public timeA = new Date()
   public time = new Date()
   public timeB!: string
   timeron: boolean = false;
-  constructor(public srv:AjaxService) { }
+  constructor(public srv: AjaxService) { }
 
   ngOnInit(): void {
-    this.srv.httppost('/api/gettimeron', {user: this.srv.user})
-    .subscribe(response => {
-      if(response!=null) {
-        this.timeA = new Date(response[0].dataon)
-        console.log('timeA:', this.timeA);
-        this.timeron = true;
-        this.timer = setInterval(() => { 
-          this.time = new Date();
-           var ms = this.time.getTime() - this.timeA.getTime()
-           
-          var seconds = Math.floor((ms / 1000) %60)
-          var minutes = Math.floor((ms / (1000 * 60)) %60)
-          var hours = Math.floor((ms / (1000 * 60 * 60)) %24)
-          hours = (hours < 10) ? Number(`0${hours}`) : hours
-          minutes = (minutes < 10) ? Number(`0${minutes}`) : minutes
-          seconds = (seconds < 10) ? Number(`0${seconds}`) : seconds
-          console.log(hours,minutes, seconds);
-          
-          this.timeB = `${hours}:${minutes}:${seconds}`
+    this.srv.httppost('/api/gettimeron', { user: this.srv.user })
+      .subscribe(response => {
+        if (response != null) {
+          this.timeA = new Date(response[0].dataon)
+          console.log('timeA:', this.timeA);
+          this.timeron = true;
+          this.timer = setInterval(() => {
+            this.time = new Date();
+            var ms = this.time.getTime() - this.timeA.getTime()
+
+            var seconds = Math.floor((ms / 1000) % 60)
+            var minutes = Math.floor((ms / (1000 * 60)) % 60)
+            var hours = Math.floor((ms / (1000 * 60 * 60)) % 24)
+            var hour: any = (hours < 10) ? `0${hours}` : hours
+            var minute: any = (minutes < 10) ? `0${minutes}` : minutes
+            var second: any = (seconds < 10) ? `0${seconds}` : seconds
+            console.log(hour, minute, second);
+
+
+            this.timeB = `${hour}:${minute}:${second}`
           }, 1000);
-      }
-      
-    })
+        }
+
+      })
   }
   public starttimer(): void {
     this.timeron = true;
     this.timeA = new Date();
-    this.srv.httppost('api/addtimeruser', {user: this.srv.user, data: this.timeA}).subscribe(response=>console.log(response))
-  this.timer = setInterval(() => { 
+    this.srv.httppost('api/addtimeruser', { user: this.srv.user, data: this.timeA }).subscribe(response => console.log(response))
+    this.timer = setInterval(() => {
       this.time = new Date();
-       var ms = this.time.getTime() - this.timeA.getTime()
-       
-      var seconds = Math.floor((ms / 1000) %60)
-      var minutes = Math.floor((ms / (1000 * 60)) %60)
-      var hours = Math.floor((ms / (1000 * 60 * 60)) %24)
-      hours = (hours < 10) ? Number(`0${hours}`) : hours
-      minutes = (minutes < 10) ? Number(`0${minutes}`) : minutes
-      seconds = (seconds < 10) ? Number(`0${seconds}`) : seconds
-      console.log(hours+":"+minutes+":"+seconds);
-      
-      this.timeB = `${hours}:${minutes}:${seconds}`
-      }, 1000);
+      var ms = this.time.getTime() - this.timeA.getTime()
+
+      var seconds = Math.floor((ms / 1000) % 60)
+      var minutes = Math.floor((ms / (1000 * 60)) % 60)
+      var hours = Math.floor((ms / (1000 * 60 * 60)) % 24)
+      var hour: any = (hours < 10) ? `0${hours}` : hours
+      var minute: any = (minutes < 10) ? `0${minutes}` : minutes
+      var second: any = (seconds < 10) ? `0${seconds}` : seconds
+      console.log(hour, minute, second);
+
+      this.timeB = `${hour}:${minute}:${second}`
+    }, 1000);
   }
   public offttimer(): void {
-    this.srv.httppost('api/deletetimeron', {user: this.srv.user}).subscribe(response=>console.log(response))
+    this.srv.httppost('api/deletetimeron', { user: this.srv.user }).subscribe(response => console.log(response))
     var req = {
-      data: `${this.myDate.getDate()}/${this.myDate.getMonth()+1}/${this.myDate.getFullYear()}`,
+      data: `${this.myDate.getDate()}/${this.myDate.getMonth() + 1}/${this.myDate.getFullYear()}`,
       timestart: this.timeA,
       timeend: this.time,
       sumtime: this.timeB,
